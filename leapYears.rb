084f5aa7f27bd3913e027247fd13bc037f04752f
Jack Stettner <jack@stet.com>
2015-07-07 23:53:23 +0000

    Updated

--- leapYears.rb
+++ leapYears.rb
@@ -1,7 +1,7 @@
 puts "Please input year 1"
 year1=gets.chomp.to_i
 puts "Please input year 2"
-year2=gets.chomp.to_i
+year2=gets.chomp.to_i+1
 difference = year2-year1
 counter = 0
 leapYears = Array.new
@@ -10,11 +10,15 @@ leapYears = Array.new
 counter = 0
 difference = year2-year1
 while counter < difference
-if (year1+counter).modulo(4) == 0 # and (year1+counter).modulo(100)!=0
+if (year1+counter).modulo(4) == 0  and (year1+counter).modulo(100)!=0
 leapYears << year1+counter
 end #end of if statement
 counter+=1
+
 end #end of while loop
 puts leapYears
 end
+puts "The leap years are as follows:"
+puts "****"
+sleep 1.0
 puts leapcalc(year1,year2)
